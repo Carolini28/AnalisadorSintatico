bae8f08737f9f40d214e4a0667444ab1b6291650
Carolini <Carolini@Carolini-PC>
2016-03-31 23:21:55 -0300

    Adicionando tratamento para componentes e corpo do programa sintatico

--- src/analisadorlexico/sintatico.java
+++ src/analisadorlexico/sintatico.java
@@ -6,6 +6,7 @@
 package analisadorlexico;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -47,15 +48,15 @@ public class sintatico {
         }
     }
     
-    //inicia a analise sintática 
     public void analisar() throws Exception{
-       try{ 
-        finalAnalise = false;
-        proxToken();
-       }
-       catch(IOException e){
-           System.out.println("");
-       }
+        
+        try{
+            finalAnalise = false;
+            proxToken();
+            inicio(null);
+        }catch(IOException e){
+            System.out.println("");
+        }
     
     }
 
@@ -74,14 +75,119 @@ public class sintatico {
         erros.add(erro);
     }
     
+    //Fazer Tratamento de erro espe4cifico do trabalho
+
+    
+    
+    //Retorna todos os erros resultantes da analise
     public Vector<String> getErro(){
     
-        Vector<String> vetorErros = new Vector<String>();
-            for(int i=0; i<erros.size();i++){
-                vetorErros.add(erros.elementAt(i).toString2());
+        Vector<String> vetor = new Vector<String>();
+        
+            for(int i=0; i<erros.size(); i++){
+                vetor.add(erros.elementAt(i).toString2());
             }
-        return vetorErros;
+        
+        return  vetor;
+        
+    }
+   
+    private List<String> UnirListas(List<String> lista1, List<String> lista2){
+        Vector uniao = new Vector();
+        
+        if(lista1 != null){
+            for(String i:lista1){
+                uniao.add(i);
+            }
+        }
+        
+        if(lista2 != null){
+            for(String i:lista2){
+            
+                if(!uniao.isEmpty()){
+                    if(!uniao.contains(i)){
+                        uniao.add(i);
+                    }
+                }else{
+                    uniao.add(i);
+                }
+            }
+        }
+        
+        return uniao.subList(0, uniao.size());
     }
     
+    //criar classe para variáveis
     
+    //Criar para tipo de variáveis
+    
+    //Inicio do programa
+    private void inicio(List<String> s) throws Exception {
+        
+        if(token.getCode().equals("program")){
+            proxToken();
+        }else{
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de program");
+        }
+        
+        if(token.getCode().equals("ID")){
+            proxToken();
+        }else{
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de ID");
+        }
+        
+        if(token.getCode().equals(";")){
+            proxToken();
+        }else{
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de ;");
+        }
+        
+        //corpo
+        
+        if(!token.getCode().equals(".")){
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de .");
+        }
+        
+        if(!analise.isTerminate()){
+            token = analise.analise();
+            if(token != null){
+               ErroSintatico("Existem simbolos após termino");
+            }
+        }else{
+            throw new IOException("Final inesperado");
+        }
+    }
+    
+    private void corpo(List<String> s) throws Exception{
+    
+        //dc_v
+        //dc_p
+        
+        if(token.getCode().equals("Begin")){
+            proxToken();
+        }else{
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de begin");
+        }
+        
+        //comandos entre begin e end
+        
+        if(token.getCode().equals("end")){
+            proxToken();
+        }else{
+            ErroSintatico("Foi encontrado "+token.getToken()+"ao invés de end");
+        }
+        
+        
+    }
+    
+    private void dc_v(List<String> s) throws Exception{
+        while(token.getCode().equals("var")){
+              proxToken();
+        }
+    }
+    
+    private void dc_p(){
+    
+    
+    }
 }
